3ba338925796f5c2bf091c9c2c2c2b5961577a17
Saint-Jean Germila <germila@noukod.com>
2019-04-02 16:37:04 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,13 +1,13 @@
 
-  
-    
-     
+
+
+
      class RepositoriesController < ApplicationController
-   
+
      def search
-   
+
      end
-   
+
      def github_search
        @resp =  Faraday.get 'https://api.github.com/search/repositories' do |req|
          req.params['q'] = params[:query]
@@ -15,10 +15,10 @@
          req.params['client_secret'] = ' 3ee6f4f1908e18d77b664c050aa23383eaa9fdd3'
          req.params['query'] = 'tetris'
        end
-   
+
        body_hash = JSON.parse(@resp.body)
        @repos = body_hash["items"]
-   
+
        render 'search'
      end
    end
